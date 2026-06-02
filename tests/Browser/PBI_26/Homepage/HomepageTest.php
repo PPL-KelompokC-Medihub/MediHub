224bdea5a3d8638e3ca4fac4ae7bd15d0a630164
rapiscode <naufalnr19@gmail.com>
2026-06-02 23:49:56 +0700

    PBI 26
    Testing PBI 8 - 13

--- tests/Browser/PBI_26/Homepage/HomepageTest.php
+++ tests/Browser/PBI_26/Homepage/HomepageTest.php
@@ -1,19 +1,18 @@
 <?php
 
-namespace Tests\Browser\PBI_26\Homepage;
+namespace Tests\Browser\PBI_26\Layanan;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class HomepagebannerTest extends DuskTestCase
+class HomepageTest extends DuskTestCase
 {
     /**
      * A Dusk test example.
      */
-    public function test_example(): void
+    private function loginPasien(Browser $browser): void
     {
-        $this->browse(function (Browser $browser) {
             $browser->visit('http://127.0.0.1:8000/login-pasien')
                 ->pause(3000)
                 ->type('@login-email', 'naufalnr19@gmail.com')
@@ -21,8 +20,17 @@ class HomepagebannerTest extends DuskTestCase
                 ->click('@login-button')
                 ->pause(3000)
                 ->waitForLocation('/pasien/beranda', 10)
-                ->assertPathIs('/pasien/beranda')
-                ->screenshot('homepage-berhasil');
+                ->assertPathIs('/pasien/beranda');
+    }
+
+    Public function test_buka_Homepage(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $this->loginPasien($browser);
+
+            $browser->clickLink('Beranda')
+                ->waitforLocation('/pasien/beranda', 10)
+                ->screenshot('beranda-berhasil');
         });
     }
 }
